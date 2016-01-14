c4abe7246090c4bcda211d98d146394d789b935e
hpatrick512 <patrick.h.512@gmail.com>
2016-01-14 15:24:41 +0100

    connexion en local
    penser à rechanger le host pour mettre le site en ligne

--- db_config.php
+++ db_config.php
@@ -1,7 +1,7 @@
 <?php
 	$DB_login = "dbo479117358"; 
 	$DB_pass = "4444fab";
-	$DB_host = "db479117358.db.1and1.com";
+	$DB_host = "localhost";
 	$DB_select="db479117358"; 
 	mysql_set_charset('UTF8');
 ?>
